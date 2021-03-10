bb680a35bbcadbf79b5a1156471d779b63351bfa
odremaer <mr.jeka0104@gmail.com>
2021-03-10 22:04:06 +0300

    devise init, feature test for "User can sign in" created, gem launchy that allows to watch the page at specific moment in feature test added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,10 @@ class QuestionsController < ApplicationController
   expose :questions, ->{ Question.all }
   expose :question
 
+  def index
+    @questions = questions
+  end
+
   def new; end
 
   def create
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #index' do
+    let(:questions) { create_list(:question, 3) }
+
+    before { get :index }
+
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+  
   describe 'GET #new' do
     before { get :new }
 
