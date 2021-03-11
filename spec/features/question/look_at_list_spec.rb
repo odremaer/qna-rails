f04a0db2faa2f78ce3f6b37c5b9c78ea84b98a77
odremaer <mr.jeka0104@gmail.com>
2021-03-11 13:49:41 +0300

    feature test "User can look at questions list" fixed

--- spec/features/question/look_at_list_spec.rb
+++ spec/features/question/look_at_list_spec.rb
@@ -5,9 +5,14 @@ feature 'User can look at questions list', %q{
   As an user/guest
   I'd like to be able to look at questions list
 } do
+  
   scenario 'user looks at questions list' do
+    questions = create_list(:question, 2)
+
     visit questions_path
 
     expect(page).to have_content 'Questions'
+    expect(page).to have_content questions[0].title
+    expect(page).to have_content questions[1].title
   end
 end
