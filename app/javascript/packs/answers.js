d3540ab6d7ef497e7db9ce50ca93cfd5fff79fd1
odremaer <mr.jeka0104@gmail.com>
2021-03-17 15:17:16 +0300

    deleting answer through #link_to instead of using event on client

--- app/javascript/packs/answers.js
+++ app/javascript/packs/answers.js
@@ -5,14 +5,4 @@ $(document).on('turbolinks:load', function(){
     let answerId = $(this).data('answerId')
     $('form#edit-answer-' + answerId).removeClass('hidden')
   })
-
-  $('.answers').on('click', '.delete-answer-link', function(e){
-    e.preventDefault()
-    let answerId = $(this).data('answerId')
-    $.ajax({
-      type: "DELETE",
-      url: "/answers/" + answerId,
-      data: {"method":"delete"},
-    })
-  })
 })
